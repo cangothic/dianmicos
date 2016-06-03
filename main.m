0b0ad469cd2260f3837bf886a95336c3835e1330
cangothic <rockangel2910@gmail.com>
2016-06-03 15:25:46 -0500

    avance 15:25

--- main.m
+++ main.m
@@ -11,3 +11,94 @@ direccion = strcat(local,'\zn2');
 addpath(direccion)
 direccion = strcat(local,'\estabilidad de un sistema discreto');
 addpath(direccion)
+while true
+    display('ingrese 1 para categoria continuo')
+    display('ingrese 2 para categoria discreto')
+    display('ingrese 3 para salir')
+    categoria = input('');
+    switch categoria
+        case 1
+            display('presione 1 para ir al menu de bode')
+            display('presione 2 para ir al menu de metodos ziegler nichols')
+            display('presione 3 para ir al los metodos del lugar de las raices')
+            categoria=input('');
+            switch categoria
+                case 1
+                    display('presione 1 para hacer una grafica de bode')
+                    display('presione 2 para criterio de estabilidad de bode')
+                    display('presione 3 para calcular el pico y frecuencia de resonancia')
+                    categoria=input('');
+
+                    switch categoria
+                        case 1
+                            num = input('ingrese el numerador de la planta');
+                            den = input('ingrese el denominador de la planta');
+                            k = input('ingrese k');
+                            planta = tf(num,den);
+                            bodeGrafica(planta,k)
+                            break
+                        case 2
+                            num = input('ingrese el numerador de la planta');
+                            den = input('ingrese el denominador de la planta');
+                            k = input('ingrese k');
+                            planta = tf(num,den);
+                            criterioBode(planta,k)
+                            break
+                        case 3
+                            num = input('ingrese el numerador de la planta');
+                            den = input('ingrese el denominador de la planta');
+                            k = input('ingrese k');
+                            planta = tf(num,den);
+                            picoYFrecuenciaDeResonancia(planta,k)
+                            break
+                    end
+
+                    break
+                case 2
+                    display('ingrese 1 para el metodo de la tangente')
+                    display('ingrese 2 para el metodo de oscilaciones sostenidas')
+                    categoria=input('');
+                    switch categoria
+                        case 1
+                            num = input('ingrese el numerador de la planta');
+                            den = input('ingrese el denominador de la planta');
+                            planta=tf(num,den);
+                            zn1(planta)
+                            break
+                        case 2
+                            num = input('ingrese el numerador de la planta');
+                            den = input('ingrese el denominador de la planta');
+                            planta=tf(num,den);
+                            zn2(planta)
+                            break
+                    end
+                    break
+            end
+            break
+        case 2
+            display('presione 1 para analizar la estabilidad de un sistema')
+            display('presione 2 para analizar la etabilidad de un sistema por routh')
+            categoria=input('');
+            switch categoria
+                case 1
+                    num = input('ingrese el numerador de la planta en dominio continuo');
+                    den = input('ingrese el denominador de la planta');
+                    planta = tf(num,den);
+                    plantadiscreta = discretizar(planta)
+                    estabilidaDiscreto(plantadiscreta)
+                    break
+                case 2
+                    num = input('ingrese el numerador de la planta en dominio continuo');
+                    den = input('ingrese el denominador de la planta');
+                    planta = tf(num,den);
+                    plantadiscreta = discretizar(planta)
+                    routhdiscreto(plantadiscreta)
+                    break
+            end
+
+            break
+    end
+    if categoria == 0
+        break
+    end
+end
